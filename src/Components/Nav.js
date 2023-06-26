8ffd8373fb288878b765231a3b10d9b8c5b10fe8
Monet <monetpaul23@gmail.com>
2023-06-26 08:28:29 -0400

    workiing on Nav

---
+++ src/Components/Nav.js
@@ -0,0 +1,17 @@
+
+function Nav (){
+return(
+<nav>
+<ul className="nav-list">
+ <li className="nav-items"><h2>Monet Paul</h2></li> 
+ <li className="nav-items"><a href="">Lauguages</a></li>
+ <li className="nav-items"><a href="">Projects</a></li>  
+ <li className="nav-items"><a href="">Contact</a></li>    
+</ul>
+</nav>
+
+)
+
+
+}
+export default Nav
