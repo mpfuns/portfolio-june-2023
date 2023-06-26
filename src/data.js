0f6c2b78d3977cd30474f5c89d452a044c942458
Monet <monetpaul23@gmail.com>
2023-06-25 20:18:43 -0400

    setup, data and images are done

--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
 export default[
     {
-    projectName:"Rich Dad Poor Dad", 
+    projectName:"Rich Dad Poor Dad",
+    picImg:"richdadpoordad.png", 
     onlineLink:"https://chic-liger-cb54db.netlify.app/",
     githubLink:"https://github.com/mpfuns/richdadpoordad-gamesheet", 
     para:"The Cashflow board game from Robert Kiyosaki, author of Rich Dad Poor Dad, inspired the project. I created a web-based spreadsheet to help players to track their income and expenses better than the game paper sheet for board game.",  
@@ -8,20 +9,23 @@ export default[
         },
     {
     projectName:"Lucid Dream Timer", 
+    picImg:"Luciddream.png",
     onlineLink:"https://poetic-gumption-fad8b0.netlify.app/",
     githubLink:"https://github.com/mpfuns/Lucid-dream-timer", 
     para:"I tried many methods to have at least one lucid dream during my spiritual journey, but none of them worked. I watch YouTube video for lucid dreaming and the guy said he had a website with this cool timer for lucid dreaming. However; I did not find the page. This lead to me deciding to make my own Lucid dream timer.",
     program:"HTML,CSS,React"     
             }, 
     {
-    projectName:"Eth Sketch", 
+    projectName:"Eth Sketch",
+    picImg:"eth-sketch.png", 
     onlineLink:"https://mpfuns.github.io/eth-a-sketch/",
     githubLink:"https://github.com/mpfuns/eth-a-sketch", 
     para:"For the eth-a-sketch project, it was one of the ultimate challenges when I was learning JavaScript from The Odin Project. I grew my searching skills from this project.",
     program:"HTML,CSS,JavaScript"    
                 }, 
     {
-    projectName:"Calculator's", 
+    projectName:"Calculator",
+    picImg:"calculator.png", 
     onlineLink:"https://beautiful-elf-180756.netlify.app/",
     githubLink:"https://github.com/mpfuns/calculator", 
     para:"For Calculator, it was the last challenge when I was learning JavaScript from The Odin Project.",
