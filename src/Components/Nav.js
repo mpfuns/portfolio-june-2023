30b1b6000a4213b30a8d1ec5f6f843fb216fbbac
Monet <monetpaul23@gmail.com>
2023-07-02 14:16:35 -0400

    finish NavBar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,9 +4,9 @@ return(
 <nav >
 <div className="my-logo">Monet Paul</div>
 <ul className="nav-list"> 
- <li className="nav-items"><a href="" className="nav-link-a">Lauguages</a></li>
- <li className="nav-items"><a href=""  className="nav-link-a">Projects</a></li>  
- <li className="nav-items"><a href=""  className="nav-link-a" >Contact</a></li>    
+ <li className="nav-items"><a href="" className="nav-link-a link-hover-effect  link-hover-effect--black">Lauguages</a></li>
+ <li className="nav-items"><a href=""  className="nav-link-a link-hover-effect  link-hover-effect--black">Projects</a></li>  
+ <li className="nav-items"><a href=""  className="nav-link-a link-a-primary" >Contact</a></li>    
 </ul>
 </nav>
 
