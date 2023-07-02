82ff7362fbb75e9119d039f23e7a163e345a73c6
Monet <monetpaul23@gmail.com>
2023-07-02 15:14:45 -0400

    set up Intro section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from './Components/Nav';
+import Intro from './Components/intro';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Intro />
     </div>
   );
 }
---
+++ src/Components/intro.js
@@ -0,0 +1,25 @@
+
+function Intro (){
+    return(
+   <div className="div-flex-c">
+    <div  className="row" >
+<div className="intro-container">
+    <figure className="intro-pic-me--mask"><img src='./thebestone.jpg' alt="Picture of Me"></img></figure>
+     <h1>Hello there! I'm <span>💕</span><span>Monet Paul</span><span>💕</span></h1>
+     <p>I’m excited to tell you a bit about myself. I’ve always been interested in technology and how it can improve people’s lives and why I pursued a career in <strong>frontend software engineering</strong>.</p>  
+     <div>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-github"></i>
+        <i class="fa-solid fa-envelope"></i>
+        <i class="fa-solid fa-file-lines"></i>
+     </div> 
+</div>
+<div><figure>Img</figure></div>
+
+    </div>
+   </div> 
+    )
+    
+    
+    }
+    export default Intro
