f16d01b90433e4f14c33f3c921ec642a06630ac5
Monet <monetpaul23@gmail.com>
2023-07-07 22:22:17 -0400

    skill comp was added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Nav from './Components/Nav';
 import Intro from './Components/intro';
+import Skills from './Components/Skills';
 import './App.css';
 import me from './Components/img/thebestone.jpg';
 import websiteImage from './Components/img/progressive.png';
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Nav/>
       <Intro me={me}  websiteImage={websiteImage}/>
+      <Skills />
       </div>
   );
 }
---
+++ src/Components/Skills.js
@@ -0,0 +1,14 @@
+function Skills (){
+    return(
+    <div>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/240px-HTML5_Badge.svg.png"/>
+      <img src="https://cdn.iconscout.com/icon/free/png-256/css-131-722685.png"/>
+      <img src="https://cdn.iconscout.com/icon/free/png-256/javascript-1-225993.png"/>
+      <img src="https://cdn.iconscout.com/icon/free/png-256/react-3-1175109.png"/>
+    </div>
+    
+    )
+    
+    
+    }
+    export default Skills
