235f6b5b3b99fd55a891bcfc8eb3980b88f9250e
Monet <monetpaul23@gmail.com>
2023-07-16 21:25:03 -0400

    update data

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export default[
     onlineLink:"https://chic-liger-cb54db.netlify.app/",
     githubLink:"https://github.com/mpfuns/richdadpoordad-gamesheet", 
     para:"The Cashflow board game from Robert Kiyosaki, author of Rich Dad Poor Dad, inspired the project. I created a web-based spreadsheet to help players to track their income and expenses better than the game paper sheet for board game.",  
-    program:"HTML,CSS,React"  
+    program:"HTML,CSS, Javascript, React"  
         },
     {
     projectName:"Lucid Dream Timer", 
@@ -13,7 +13,7 @@ export default[
     onlineLink:"https://poetic-gumption-fad8b0.netlify.app/",
     githubLink:"https://github.com/mpfuns/Lucid-dream-timer", 
     para:"I tried many methods to have at least one lucid dream during my spiritual journey, but none of them worked. I watch YouTube video for lucid dreaming and the guy said he had a website with this cool timer for lucid dreaming. However; I did not find the page. This lead to me deciding to make my own Lucid dream timer.",
-    program:"HTML,CSS,React"     
+    program:"HTML,CSS, Javascript, React"     
             }, 
     {
     projectName:"Eth Sketch",
